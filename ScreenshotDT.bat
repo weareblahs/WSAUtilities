a2a42b21d33dad67b9264847b95f7e676bfe7877
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2021-10-28 20:59:59 +0800

    Screenshot 1.0

---
+++ ScreenshotDT.bat
@@ -0,0 +1,27 @@
+@echo off
+
+echo Windows Subsystem for Android Screenshot tool
+echo This script screenshots your current WSA screen. Currently supported if running 1 app.
+echo Please make sure that ADB (Developer Tools) is enabled.
+echo If enabled, press any key.
+pause >nul
+goto start
+
+:start
+set /P png=Enter directory on where you want to screenshot:
+:prestart
+echo When you're ready, press any key to start screenshot.
+pause >nul
+
+:: There you go. Adjust the properties below as you want.
+adb exec-out screencap -p > "%date:~4,2%%date:~7,2%%date:~10,4% %time:~0,2%%time:~3,2%%time:~6,2%.png"
+goto end
+
+:end
+echo Screenshot capture complete!
+set /p var=Do you want to take another screenshot? [Yes (Y) / No (N) / Without output prompt (W)]: 
+if %var%== Yes GOTO start
+if %var%== yes GOTO start
+if %var%== Y GOTO start
+if %var%== W GOTO prestart
+if not %var%== No exit
