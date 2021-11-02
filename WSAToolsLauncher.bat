d370331d2d2680b8d2cf803d00b07ec641107441
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2021-11-02 16:12:13 +0800

    WSAToolsLauncher from WSATools 0.0.7
     - Fix directory

--- WSAToolsLauncher.bat
+++ WSAToolsLauncher.bat
@@ -1,17 +1,21 @@
 @echo off
+:: Start text
 echo WSATools launcher
 echo Windows Subsystem for Android requires the latest version of Windows 11. Please wait as we check that if your PC is running the latest version.
 del checkver.bat
+:: Move ver content to text file
 ver >> checkver.bat
-fart checkver.bat "Microsoft Windows [Version" " "
-fart checkver.bat "]" " "
-fart -C checkver.bat \r\n " "
-fart -C checkver.bat "   " "@echo off \r\n echo "
-fart checkver.bat "  " " "
-fart -C checkver.bat ".282 " ".282\r\n"
-fart checkver.bat " echo " "echo "
+:: Replace terms, making the batch file return "echo (version)"
+main/fart checkver.bat "Microsoft Windows [Version" " "
+main/fart checkver.bat "]" " "
+main/fart -C checkver.bat \r\n " "
+main/fart -C checkver.bat "   " "@echo off \r\n echo "
+main/fart checkver.bat "  " " "
+main/fart -C checkver.bat ".282 " ".282\r\n"
+main/fart checkver.bat " echo " "echo "
 for /f %%N in ('checkver') do set "check=%%N"
 if %check% == 10.0.22000.282 (
+:: Success screen
   cls
   echo Windows version up-to-date, which means it supports Windows Subsystem for Android. 
   echo Launching in 3 seconds...
@@ -30,6 +34,7 @@ if %check% == 10.0.22000.282 (
   WSATools
   pause >nul
 ) else (
+:: Failed screen
   cls
   echo Windows version not supported for Windows Subsystem for Android.
   echo Your current Windows build is %check% meanwhile WSATools needs build 10.0.22000.282.
