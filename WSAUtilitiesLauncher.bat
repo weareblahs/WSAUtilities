01520a524291a36b2246c595d5839067baf685db
thisistyx-AUTO <tanyuxuan2005@gmail.com>
2021-12-10 01:15:54 +0800

    v0.0.9 rev2

--- WSAUtilitiesLauncher.bat
+++ WSAUtilitiesLauncher.bat
@@ -1,7 +1,27 @@
 @echo off
+:: Lang import
+for /f "delims=" %%i in ('main\LocalVariables main\WSAUtilities.ini Localization Language') do set lang=%%i >nul
+for /f "delims=" %%i in ('main\LocalVariables main\lang\%lang:~0,2%.ini WSAUtilitiesLauncher intro1') do set intro1=%%i >nul
+for /f "delims=" %%i in ('main\LocalVariables main\lang\%lang:~0,2%.ini WSAUtilitiesLauncher intro2') do set intro2=%%i >nul
+for /f "delims=" %%i in ('main\LocalVariables main\lang\%lang:~0,2%.ini WSAUtilitiesLauncher supported1') do set supported1=%%i >nul
+for /f "delims=" %%i in ('main\LocalVariables main\lang\%lang:~0,2%.ini WSAUtilitiesLauncher s1') do set 3s=%%i >nul
+for /f "delims=" %%i in ('main\LocalVariables main\lang\%lang:~0,2%.ini WSAUtilitiesLauncher s2') do set 2s=%%i >nul
+for /f "delims=" %%i in ('main\LocalVariables main\lang\%lang:~0,2%.ini WSAUtilitiesLauncher s3') do set 1s=%%i >nul
+for /f "delims=" %%i in ('main\LocalVariables main\lang\%lang:~0,2%.ini WSAUtilitiesLauncher error1') do set failed1=%%i >nul
+for /f "delims=" %%i in ('main\LocalVariables main\lang\%lang:~0,2%.ini WSAUtilitiesLauncher error2') do set failed2=%%i >nul
+for /f "delims=" %%i in ('main\LocalVariables main\lang\%lang:~0,2%.ini WSAUtilitiesLauncher error3') do set failed3=%%i >nul
+for /f "delims=" %%i in ('main\LocalVariables main\lang\%lang:~0,2%.ini WSAUtilitiesLauncher error4') do set failed4=%%i >nul
+for /f "delims=" %%i in ('main\LocalVariables main\lang\%lang:~0,2%.ini WSAUtilitiesLauncher error5') do set failed5=%%i >nul
+for /f "delims=" %%i in ('main\LocalVariables main\lang\%lang:~0,2%.ini WSAUtilitiesLauncher error6') do set failed6=%%i >nul
+
+:: Version import
+for /f "delims=" %%i in ('main\LocalVariables main\WSAUtilities.ini Launcher Current') do set current=%%i >nul
+for /f "delims=" %%i in ('main\LocalVariables main\WSAUtilities.ini Launcher Devbuild') do set devbuild=%%i >nul
+
+
 :: Start text
-echo WSAUtilities launcher
-echo Windows Subsystem for Android requires the latest version of Windows 11. Please wait as we check that if your PC is running the latest version.
+echo %intro1%
+echo %intro2%
 del checkver.bat
 :: Move ver content to text file
 ver >> checkver.bat
@@ -13,36 +33,54 @@ main\fart -C checkver.bat "   " "@echo off \r\n echo " >nul
 main\fart checkver.bat "  " " " >nul
 main\fart -C checkver.bat ".348 " ".348\r\n" >nul
 main\fart checkver.bat " echo " "echo " >nul
-for /f %%N in ('checkver') do set "check=%%N"
-for /f %%N in ('echo %check:~5,5%') do set "check2=%%N"
-if %check2% == 22000 (
+for /f "delims=" %%i in ('checkver') do set check=%%i >nul
+for /f "delims=" %%i in ('echo %check:~5,5%') do set check2=%%i >nul
+if %check2% == %current% (
 :: Success screen
   cls
-  echo Windows version up-to-date, which means it supports Windows Subsystem for Android. 
-  echo Launching in 3 seconds...
+  echo %supported1%
+  echo %3s%
   timeout 1 > NUL
   cls
-  echo Windows version up-to-date, which means it supports Windows Subsystem for Android. 
-  echo Launching in 2 seconds...
+  echo %supported1%
+  echo %2s%
   timeout 1 > NUL
   cls
-  echo Windows version up-to-date, which means it supports Windows Subsystem for Android. 
-  echo Launching in 1 seconds...
+  echo %supported1%
+  echo %1s%
   timeout 1 > NUL
   del checkver.bat
   cls
   cd main
   WSAUtilities
-  pause >nul
+)
+if %check2% == %devbuild% (
+:: Success screen
+  cls
+  echo %supported1%
+  echo %3s%
+  timeout 1 > NUL
+  cls
+  echo %supported1%
+  echo %2s%
+  timeout 1 > NUL
+  cls
+  echo %supported1%
+  echo %1s%
+  timeout 1 > NUL
+  del checkver.bat
+  cls
+  cd main
+  WSAUtilities
 ) else (
 :: Failed screen
   cls
-  echo Windows version not supported for Windows Subsystem for Android.
-  echo Your current Windows build is %check% meanwhile WSAUtilities needs Windows 11 to run.
-  echo Please check for Windows updates, update it and try again. This detection script detects for
-  echo the newest build of Windows 11. You can also try to run the script by going to the "main"
-  echo folder then find "WSAUtilities.bat".
-  echo Press any key to exit.
+  echo %failed1%
+  echo %failed2%
+  echo %failed3%
+  echo %failed4%
+  echo %failed5%
+  echo %failed6%
   del checkver.bat
   pause >nul
   cls
