7018953f55aeb4cb2b79f7f89cd27e869c1e3e28
Tan Yu Xuan <37889443+weareblahs@users.noreply.github.com>
2022-03-13 23:51:37 +0800

    DeviceCheck

--- python/DeviceCheck.py
+++ python/DeviceCheck.py
@@ -9,10 +9,10 @@ userVersion = platform.version()
 
 
 if currentBuild == userVersion:
-    print("Build is " + currentBuild)
+    print("You are running a stable build of Windows 11, that means you can run Windows Subsystem for Android.")
 
 if currentBuild > userVersion:
-    print(")
+    print("You are running an insider build of Windows 11, that means your computer can run Windows Subsystem for Android and it can also be downloaded on Windows Store without using InstallWSA.")
 
 else
     print("Device not supported for Windows Subsystem for Android. Make sure you installed a stable version of Windows 11 or Windows 11 Insider Builds.")
